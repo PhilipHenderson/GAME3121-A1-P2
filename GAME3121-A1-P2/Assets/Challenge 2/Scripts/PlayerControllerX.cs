6cd45a49e1dde52ffc9e1282587f133a64d877bf
Philip Henderson <philip.henderson@hotmail.com>
2023-09-27 13:41:02 -0400

    Initial Commit, Part2 Functionality Complete, About to record

---
+++ GAME3121-A1-P2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if there are any active dog objects in the scene
+        GameObject[] dogObjects = GameObject.FindGameObjectsWithTag("Dog");
+
+        // On spacebar press and no active dog objects are found, send a dog
+        if (Input.GetKeyDown(KeyCode.Space) && dogObjects.Length == 0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
